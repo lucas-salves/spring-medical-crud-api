38e4a8c86fd53e448e75ce1080c33d8b5cef0f44
lucas-salves <salves.lucas@outlook.com>
2022-04-10 03:01:47 -0300

    fix: add @GeneratedValue annotation
    to make the primary key auto-increment

--- DoctorsService/DoctorsService/src/main/java/com/gcb/DoctorsService/entity/Specialty.java
+++ DoctorsService/DoctorsService/src/main/java/com/gcb/DoctorsService/entity/Specialty.java
@@ -3,6 +3,8 @@ package com.gcb.DoctorsService.entity;
 import com.gcb.DoctorsService.model.MedicalSpecialty;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
@@ -13,6 +15,7 @@ import lombok.Data;
 public class Specialty {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     
     @Column(nullable = false, unique = false)
