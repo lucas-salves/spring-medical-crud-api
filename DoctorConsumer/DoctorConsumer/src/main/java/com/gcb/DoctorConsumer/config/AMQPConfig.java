e5191944b454890c7638d91a785a4ced7a6bec9f
lucas-salves <salves.lucas@outlook.com>
2022-04-11 19:20:10 -0300

    build: Add the RabbitMQ dependency

---
+++ DoctorConsumer/DoctorConsumer/src/main/java/com/gcb/DoctorConsumer/config/AMQPConfig.java
@@ -0,0 +1,54 @@
+package com.gcb.DoctorsService.config;
+
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AMQPConfig {
+    
+    private static final String URI = "AWS URI";
+
+    private static final String USERNAME = "rabbitUser";
+
+    private static final String PASSWORD = "rabbitPassword";
+
+    public static Connection getConnection() {
+        
+        Connection connection = null;
+        
+        try {
+            var factory = new ConnectionFactory();
+            
+            factory.setUri(URI);
+            factory.setConnectionTimeout(5000);
+            factory.setRequestedHeartbeat(5);
+            factory.setUsername(USERNAME);
+            factory.setPassword(PASSWORD);
+            
+            var exec = Executors.newCachedThreadPool();
+            
+            connection = factory.newConnection(exec);
+            
+            return connection;
+        } catch (IOException ex) {
+            Logger.getLogger(AMQPConfig.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TimeoutException ex) {
+            Logger.getLogger(AMQPConfig.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(AMQPConfig.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(AMQPConfig.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (KeyManagementException ex) {
+            Logger.getLogger(AMQPConfig.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return connection;
+    }
+}
---
+++ DoctorConsumer/DoctorConsumer/src/main/java/com/gcb/DoctorConsumer/service/AMQPPublisher.java
@@ -0,0 +1,52 @@
+package com.gcb.DoctorsService.service;
+
+import com.gcb.DoctorsService.config.AMQPConfig;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class AMQPPublisher {
+
+    private Connection connection;
+
+    private Channel channel;
+
+    public AMQPPublisher() {
+        try {
+            connection = AMQPConfig.getConnection();
+            channel = connection.createChannel();
+        } catch (IOException ex) {
+            Logger.getLogger(AMQPPublisher.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void sendToQueue(String queue, String message) throws IOException {
+        if (connection == null || channel == null) {
+            connection = AMQPConfig.getConnection();
+            channel = connection.createChannel();
+        }
+
+        //queue, durable, exclusive, autoDelete, properties
+        channel.queueDeclare(queue, true, false, false, null);
+
+        channel.basicPublish("", queue, null, message.getBytes("UTF-8"));
+    }
+
+    public void close() {
+        //replace with a try-with-resources later
+        try {
+            channel.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
