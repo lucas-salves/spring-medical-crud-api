6e9b752513feed6ea37542b00c4adf30e79b7d4c
lucas-salves <salves.lucas@outlook.com>
2022-04-13 17:28:43 -0300

    feat: Add the /getAll feed endpoint method
    That method is responsible to return all feeds

--- FeedService/FeedService/src/main/java/com/gcb/FeedService/controller/FeedController.java
+++ FeedService/FeedService/src/main/java/com/gcb/FeedService/controller/FeedController.java
@@ -2,11 +2,16 @@ package com.gcb.FeedService.controller;
 
 import com.gcb.FeedService.entity.Feed;
 import com.gcb.FeedService.repository.FeedRepository;
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,27 +26,44 @@ public class FeedController {
 
     @PostMapping(value = "/createFeed")
     public ResponseEntity<Feed> createFeed(@RequestBody Feed feed) {
-        
+
         feed.setRequestAction("createFeed");
-        
+
         feed.setStatus("Queued");
-        
+
         feed.setCreatedAt(new Date().toString());
-        
+
         try {
-            
+
             repository.save(feed);
-            
+
         } catch (Exception ex) {
-            
+
             feed.setFeedErrors(true);
-            
+
             feed.setMessageError("Erro desconhecido.");
-            
+
             return new ResponseEntity<Feed>(feed, HttpStatus.INTERNAL_SERVER_ERROR);
-            
+
         }
         return new ResponseEntity<Feed>(feed, HttpStatus.CREATED);
     }
-    
+
+    @GetMapping(value = "/getAll")
+    public ResponseEntity<List<Feed>> listAll() throws Exception {
+        
+        try {
+            
+            Iterable feedsResponse = repository.findAll();
+            
+            List<Feed> feeds = ImmutableList.copyOf(feedsResponse);
+            
+            return new ResponseEntity<>(feeds, HttpStatus.OK);
+        } catch (Exception ex) {
+            throw  new Exception(ex.getMessage());
+        }
+
+
+    }
+
 }
