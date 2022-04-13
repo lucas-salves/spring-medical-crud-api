15b1285b048d405b1358d3c73f6e0944eb6c0ec8
lucas-salves <salves.lucas@outlook.com>
2022-04-13 01:49:39 -0300

    feat: Add the FeedService
    The feed service is responsible to return the status of the async proccessing

---
+++ FeedService/FeedService/src/test/java/com/gcb/FeedService/FeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcb.FeedService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
